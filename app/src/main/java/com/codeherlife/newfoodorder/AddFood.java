212a52d9ab1af4f9cf723cc468dd0a1c732d22ed
codeherlife <codeherlife@gmail.com>
2018-04-08 18:35:03 -0700

    added buttons and functionality for item description and price and name

--- app/src/main/java/com/codeherlife/newfoodorder/AddFood.java
+++ app/src/main/java/com/codeherlife/newfoodorder/AddFood.java
@@ -2,12 +2,22 @@ package com.codeherlife.newfoodorder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class AddFood extends AppCompatActivity {
+
+    private ImageButton foodImage;
+    private static final int GALLREQ = 1;
+    private EditText name, desc, price;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_food);
+
+        name = (EditText) findViewById(R.id.itemName);
+        desc = (EditText) findViewById(R.id.itemDesc);
+        price = (EditText) findViewById(R.id.itemPrice);
     }
 }
