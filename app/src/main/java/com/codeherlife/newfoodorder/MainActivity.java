7c2c542c3e7d23b3305367168318e9aac0caa83d
codeherlife <codeherlife@gmail.com>
2018-04-08 18:07:20 -0700

    added activity for adding food items- manager's view

--- app/src/main/java/com/codeherlife/newfoodorder/MainActivity.java
+++ app/src/main/java/com/codeherlife/newfoodorder/MainActivity.java
@@ -1,5 +1,6 @@
 package com.codeherlife.newfoodorder;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -26,6 +27,11 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    public void addFoodButtonClicked(View view){
+        Intent addFoodIntent = new Intent(MainActivity.this, AddFood.class);
+        startActivity(addFoodIntent);
     }
 
     @Override
