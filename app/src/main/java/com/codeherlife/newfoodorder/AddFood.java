8e855a373b61718292ff7680817032b04e933e84
codeherlife <codeherlife@gmail.com>
2018-04-08 18:51:25 -0700

    added ability to add images and functionality

--- app/src/main/java/com/codeherlife/newfoodorder/AddFood.java
+++ app/src/main/java/com/codeherlife/newfoodorder/AddFood.java
@@ -1,5 +1,6 @@
 package com.codeherlife.newfoodorder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
@@ -10,6 +11,7 @@ public class AddFood extends AppCompatActivity {
     private ImageButton foodImage;
     private static final int GALLREQ = 1;
     private EditText name, desc, price;
+    private Uri uri = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +21,23 @@ public class AddFood extends AppCompatActivity {
         name = (EditText) findViewById(R.id.itemName);
         desc = (EditText) findViewById(R.id.itemDesc);
         price = (EditText) findViewById(R.id.itemPrice);
+
+    }
+
+    public void imageButtonClicked(View view){
+
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("Image/*");
+        startActivityForResult(galleryIntent, GALLREQ);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GALLREQ && resultCode == RESULT_OK){
+            uri = data.getData();
+            foodImage = (ImageButton) findViewById(R.id.foodImageButton);
+            foodImage.setImageURI(uri);
+        }
     }
 }
